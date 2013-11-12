bae0e9261c96f52cf835f200bcf5dddeec5e422e
Johan Wennerberg <johan.x.wennerberg@ericsson.com>
2013-11-12 14:48:19 +0100

    Remove apache user config

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,26 +3,21 @@
 # Module to manage ViewVC
 #
 class viewvc (
-  $package        = 'viewvc',
-  $config_path    = '/etc/viewvc/viewvc.conf',
-  $config_mode    = '0644',
-  $config_owner   = 'root',
-  $config_group   = 'root',
-  $vhost_port     = '80',
-  $vhost_docroot  = '/var/www/viewvc',
-  $vhost_template = 'viewvc/vhost.conf.erb',
-  $apache_user    = 'USE_DEFUALT',
-  $apache_group   = 'USE_DEFUALT',
-  $root_parents   = undef,
+  $package           = 'viewvc',
+  $config_path       = '/etc/viewvc/viewvc.conf',
+  $config_mode       = '0644',
+  $config_owner      = 'root',
+  $config_group      = 'root',
+  $vhost_port        = '80',
+  $vhost_docroot     = '/var/www/viewvc',
+  $vhost_template    = 'viewvc/vhost.conf.erb',
+  $conf_root_parents = undef,
 ) {
 
   case $::osfamily {
     'RedHat' : {
       case $::lsbmajdistrelease {
-        '6': {
-          $default_apache_user = 'apache'
-          $default_apache_group = 'apache'
-        }
+        '6': { }
         default: {
           fail("viewvc is supported on EL 6. Your osfamily and lsbmajdistrelease identified as ${::osfamily} ${::lsbmajdistrelease}.")
         }
@@ -34,21 +29,6 @@ class viewvc (
   }
 
   include apache
-
-  if $apache_user == 'USE_DEFAULT' {
-    $my_apache_user = $default_apache_user
-  } else {
-    $my_apache_user = $apache_user
-  }
-
-  if $apache_group == 'USE_DEFAULT' {
-    $my_apache_group = $default_apache_group
-  } else {
-    $my_apache_group = $apache_group
-  }
-
-  $apache::params::user = $my_apache_user
-  $apache::params::group = $my_apache_group
 
   package { 'viewvc_package':
     ensure => installed,
